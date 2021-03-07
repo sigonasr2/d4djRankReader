01c97f629cee908e240e0fd26d93d2667b6f0e60
Joshua Sigona <sigonasr2@gmail.com>
2021-03-07 17:05:39 +0900

    Add 1000 limit

--- src/main/java/d4dj/d4dj/App.java
+++ src/main/java/d4dj/d4dj/App.java
@@ -348,7 +348,7 @@ public class App
         					break;
         				}
         			}
-        			if (!found&&scores.size()<ranks.length) {
+        			if (!found&&scores.size()<ranks.length&&thisScore!=1000) {
         				scores.add(thisScore);
         			}
         		} else {
