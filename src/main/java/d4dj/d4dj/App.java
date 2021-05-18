05af402170cbc496fc4361db8325efc46134c1d3
Joshua Sigona <sigonasr2@gmail.com>
2021-05-18 12:00:59 +0900

    Muni

--- src/main/java/d4dj/d4dj/App.java
+++ src/main/java/d4dj/d4dj/App.java
@@ -112,7 +112,7 @@ class SubmitThread implements Runnable{
 public class App 
 {
 	public static Image paragraph/*,paragraph2,paragraph3*/;
-	final static int EVENT = 18;
+	final static int EVENT = 19;
 	public static int[] lastScores = new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 	public static int[] lastLowerTierScores = new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 	public static Font myFont = new Font("Serif", Font.BOLD, 26);
